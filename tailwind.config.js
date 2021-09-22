13b8b2dd0e729e6205561d550ecc31f09c5af737
younoch <younoch295@outlook.com>
2021-09-22 21:52:51 +0600

    Setup Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,16 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    debugScreens: {
+      position: ['top', 'left'],
+    },
     extend: {},
   },
   variants: {
+
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-debug-screens'),
+  ],
 }
